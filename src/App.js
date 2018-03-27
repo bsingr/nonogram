fc63aff9d614ec9891d8354fdca6970a0903d41d
Jens Bissinger <mail@jens-bissinger.de>
2018-03-27 21:55:28 +0200

    Add padding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class EmptyHint extends Component {
+  render() {
+    return (
+      <div className="Field EmptyHint" style={{height: `${this.props.height}px`}}>
+        -
+      </div>
+    );
+  }
+}
+
 class Hint extends Component {
   render() {
     return (
@@ -23,13 +33,11 @@ class Field extends Component {
 
 function createMap(x,y) {
   const map = []
-  for (let i = 0; i < y; i++) {
-    const row = [];
-    for (let j = 0; j < x; j++) {
-      row.push(Math.random() > 0.5 ? 1 : 0)
-    }
-    map.push(row)
+  for (let i = 0; i < (x * y); i++) {
+    map.push(Math.random() > 0.5 ? 1 : 0)
   }
+  map.x = x
+  map.y = y
   return map;
 }
 
@@ -51,9 +59,45 @@ function hints(streak) {
   return hints
 }
 
+function rows(map) {
+  const rows = []
+  map.forEach((node, idx) => {
+    if (idx % map.x === 0) {
+      rows.push([])
+    }
+    rows[rows.length - 1].push(node)
+  })
+  return rows
+}
+
+// x=3
+// y=2
+//
+// [0,1,2,3,4,5]
+//
+// rows
+// [[0,1,2]
+//  [3,4,5]]
+//
+// cols
+// [[0,3],
+//  [1,4],
+//  [2,5]]
+function cols(map) {
+  const cols = []
+  for (let i = 0; i < map.x; i++) {
+    const col = []
+    cols.push(col)
+  }
+  map.forEach((node, idx) => {
+    cols[idx % map.x].push(node)
+  })
+  return cols
+}
+
 function maxHorizontalHints(map) {
   let max = 0;
-  map.forEach(row => {
+  rows(map).forEach(row => {
     const current = hints(row).length
     if (current > max) {
       max = current
@@ -62,19 +106,24 @@ function maxHorizontalHints(map) {
   return max
 }
 
-function rotateMap(map) {
-  const rotateMap = [];
-  map.forEach(row => {
-    row.forEach(() => rotateMap.push([]))
-    row.forEach((node, nodeIdx) => {
-      rotateMap[nodeIdx].push(node)
-    })
+function maxVerticalHints(map) {
+  let max = 0;
+  cols(map).forEach(col => {
+    const current = hints(col).length
+    if (current > max) {
+      max = current
+    }
   })
-  return rotateMap;
+  return max
 }
 
-function maxVerticalHints(map) {
-  return maxHorizontalHints(rotateMap(map))
+function fillLeft(streak, size) {
+  const missingSize = size - streak.length
+  const missing = [];
+  for (let i = 0; i < missingSize; i++) {
+    missing.push(undefined)
+  }
+  return missing.concat(streak);
 }
 
 class App extends Component {
@@ -83,14 +132,19 @@ class App extends Component {
     const xFields = 3;
     const map = createMap(xFields,yFields)
     const yHints = maxVerticalHints(map);
+    const xHints = maxHorizontalHints(map);
     const fieldHeight = window.innerHeight / (yHints + yFields);
     return (
       <div className="App">
-        {map.map((row, rowIdx) => {
+        {rows(map).map((row, rowIdx) => {
           return (
             <div key={rowIdx} className="Row">
-              {hints(row).map((hint, hintIdx) => {
-                return <Hint key={hintIdx} height={fieldHeight} value={hint} />;
+              {fillLeft(hints(row), xHints).map((hint, hintIdx) => {
+                if (hint) {
+                  return <Hint key={hintIdx} height={fieldHeight} value={hint} />;
+                } else {
+                  return <EmptyHint key={hintIdx} height={fieldHeight} />;
+                }
               })}
               {row.map((node, nodeIdx) => {
                 return <Field key={nodeIdx} height={fieldHeight} value={node} />;
@@ -104,5 +158,9 @@ class App extends Component {
 }
 
 App.hints = hints;
+App.createMap = createMap;
+App.rows = rows;
+App.cols = cols;
+App.fillLeft = fillLeft;
 
 export default App;
--- src/model.test.js
+++ src/model.test.js
@@ -6,3 +6,25 @@ it('generates hints', () => {
   expect(App.hints([1,0,1,0,1])).toEqual([1,1,1])
   expect(App.hints([1,0,1,1,1])).toEqual([1,3])
 });
+
+it('gets rows', () => {
+  const map = App.createMap(2,3)
+  expect(App.rows(map)).toEqual([
+    [map[0], map[1]],
+    [map[2], map[3]],
+    [map[4], map[5]]
+  ])
+});
+
+it('gets cols', () => {
+  const map = App.createMap(2,3)
+  expect(App.cols(map)).toEqual([
+    [map[0], map[2], map[4]],
+    [map[1], map[3], map[5]]
+  ])
+});
+
+it('gets fillLeft', () => {
+  expect(App.fillLeft([0,1,2], 3)).toEqual([0,1,2])
+  expect(App.fillLeft([0,1,2], 5)).toEqual([undefined,undefined,0,1,2])
+});
